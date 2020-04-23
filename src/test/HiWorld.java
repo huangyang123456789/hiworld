7a0ebd440591db5dcae06c110299d1e6508b3f0f
huangyang123456789 <1251446477@qq.com>
2020-04-23 13:59:37 +0800

    test1

---
+++ src/test/HiWorld.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @Author: wwb
+ * @Date: 2020/4/23 13:53
+ */
+public class HiWorld {
+    public static void main(String[] args) {
+        System.err.println("HiWorld");
+    }
+}
